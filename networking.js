20e0bebc7ab62ab33e6ab7c73199f71bb6971b04
kevinmalby <kevin.malby@gmail.com>
2015-02-16 13:52:41 -0800

    Fixed missing parenthesis in setTimeout and reformatted

--- networking.js
+++ networking.js
@@ -4,43 +4,44 @@ function receiveMessage(payload) {
   serverData = JSON.parse(payload);
 
   switch (serverData.phase) {
-  case "initialization":
+    case "initialization":
       initialize();
       break;
-  case "initial_ball_position":
-      initializeBall(serverData.ball_position,serverData.ball_size);
+    case "initial_ball_position":
+      initializeBall(serverData.ball_position, serverData.ball_size);
       var json = {
-          "phase":"ready_to_start",
-          "ball_position":[ball.x, ball.y]
+        "phase": "ready_to_start",
+        "ball_position": [ball.x, ball.y]
       }
       server.send('message', JSON.stringify(json));
       break;
-  case "send_info":
-      server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val()  + "\" \}");
+    case "send_info":
+      server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
       break;
-  case "wait":
+    case "wait":
       // ping back with a ready message in 2 seconds
-      setTimeout(2000, function(){server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val()  + "\" \}");
-      subBtn.text("Waiting for Partner"); 
-
+      setTimeout(2000, function() {
+        server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
+        subBtn.text("Waiting for Partner");
+      });
       break;
-  case "set_opponent":
+    case "set_opponent":
       setOpponent(serverData.name);
-      break;    
-  case "start":
+      break;
+    case "start":
       // Show start animation
       // don't know if keeping this, but good for now
-      canvas.style.visibility="visible";
+      canvas.style.visibility = "visible";
       $('#stats-header').show();
       $('#stats-container').show();
-      subBtn.text("Disconnect"); 
-      
+      subBtn.text("Disconnect");
+
       setInterval(updatePaddle, 100);
       break;
-  case "score_update":
+    case "score_update":
       setScore(serverData.new_score, serverData.num_tries);
       break;
-  case "ball_update":
+    case "ball_update":
       var pos = serverData.ball_position;
       pos[1] = pos[1] + (topWallRect.y + topWallRect.height);
       setBallPosition(pos);
@@ -48,10 +49,11 @@ function receiveMessage(payload) {
   }
 }
 
+
 function initialize() {
   pongApp.showPleaseWait();
   var initialData = {
-    "phase":"initial_dimensions",
+    "phase": "initial_dimensions",
     "map_dimensions": [offsetX,
       offsetY + (topWallRect.y + topWallRect.height),
       farWallRect.x, bottomWallRect.y - (topWallRect.y + topWallRect.height)
@@ -84,15 +86,15 @@ function connect(ipAddress, port, playerName) {
 
   // //Log any messages sent from server
   server.bind('message', receiveMessage);
-  
+
   server.connect();
   /* This send message needs to be somehwere else too, server
   is still in connecting state at this point*/
 }
 
-function disconnect(ipAddress, port, playerName){
+function disconnect(ipAddress, port, playerName) {
 
-    server.send("message", "{\"phase\":\"disconnect\"}");
-    server.disconnect();
+  server.send("message", "{\"phase\":\"disconnect\"}");
+  server.disconnect();
 
 }
