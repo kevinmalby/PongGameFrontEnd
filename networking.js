be16ea4551c09b7df55f798d9fdedb9cd2aef71f
Kathryn Rodgers <rodgersk@uci.edu>
2015-02-17 22:40:24 -0800

    Disabled submit button so ppl can't connect twice. Added disconnected phase in messageHandler. :D

--- networking.js
+++ networking.js
@@ -1,145 +1,156 @@
 var server;
+var updatePaddleIntervalID;
 
 function receiveMessage(payload) {
-  serverData = JSON.parse(payload);
-
-  switch (serverData.phase) {
+    serverData = JSON.parse(payload);
+    var json;
+    switch (serverData.phase) {
     case "initialization":
-      initialize(serverData.player_number);
-      break;
+	initialize(serverData.player_number);
+	$("#header-submit").addClass('disabled');
+	break;
     case "initial_ball_position":
-      initializeBall(serverData.ball_position, serverData.ball_size);
-      var json = {
-        "phase": "ready_to_start",
-        "name": playerName,
-        "ball_position": [ball.x, ball.y]
-      };
-      server.send('message', JSON.stringify(json));
-      break;
+	initializeBall(serverData.ball_position, serverData.ball_size);
+	json = {
+            "phase": "ready_to_start",
+            "name": playerName,
+            "ball_position": [ball.x, ball.y]
+	};
+	server.send('message', JSON.stringify(json));
+	break;
     case "send_info":
-      var json = {
-        "phase": "exchange_info",
-        "name": playerName
-      };
-      server.send('message', JSON.stringify(json));
-      break;
+	json = {
+            "phase": "exchange_info",
+            "name": playerName
+	};
+	server.send('message', JSON.stringify(json));
+	break;
     case "wait":
-      $('#loading-container').show();
-      //server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
-      //setTimeout(100, function() {
-      //server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").text() + "\" }");
-      //subBtn.text("Ping!");
-      //}  
-      //);
-      // ping back with a ready message in 2 seconds
-      // setTimeout(2000, function() {
-      //   server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
-      //   subBtn.text("Waiting for Partner");
-      // });
-      break;
+	$('#loading-container').show();
+	//server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
+	//setTimeout(100, function() {
+	//server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").text() + "\" }");
+	//subBtn.text("Ping!");
+	//}  
+	//);
+	// ping back with a ready message in 2 seconds
+	// setTimeout(2000, function() {
+	//   server.send('message', "\{\"phase\":\"exchange_info\", \"name\":\"" + $("#player-name-input").val() + "\" \}");
+	//   subBtn.text("Waiting for Partner");
+	// });
+	break;
     case "set_opponent":
-      setOpponent(serverData.name);
-      $('#loading-container').hide();
-      break;
+	setOpponent(serverData.name);
+	$('#loading-container').hide();
+	break;
     case "start":
-      // Show start animation
-      // don't know if keeping this, but good for now
-      canvas.style.visibility = "visible";
-      $('#stats-header').show();
-      $('#stats-container').show();
-      subBtn.text("Disconnect");
+	// Show start animation
+	// don't know if keeping this, but good for now
+	canvas.style.visibility = "visible";
+	$('#stats-header').show();
+	$('#stats-container').show();
+	subBtn.removeClass("disabled");
+	subBtn.text("Disconnect");
+	
+	updatePaddleIntervalID = setInterval("updatePaddle();", 100);
+	console.log("updateID: " + updatePaddleIntervalID);
+	break;
+    case "disconnected":	
+	var ipAddress = $("#ip-address-input").val();
+	var port = $("#port-number-input").val();
 
-      setInterval(updatePaddle, 100);
-      break;
+	disconnect(ipAddress, port, playerName);
+	break;
     case "score_update":
-      setScore(serverData.new_score, serverData.num_tries);
-      break;
+	setScore(serverData.new_score, serverData.num_tries);
+	break;
     case "opponent_paddle_update":
-      var opponentPaddlePos = serverData.opponent_paddle;
-      if (playerNum == 0) {
-        setOpponentPaddle(opponentPaddlePos[0], opponentPaddlePos[1])
-      } else {
-        setOpponentPaddle(opponentPaddlePos[0] + 984, opponentPaddlePos[1])
-      }
+	var opponentPaddlePos = serverData.opponent_paddle;
+	if (playerNum == 0) {
+            setOpponentPaddle(opponentPaddlePos[0], opponentPaddlePos[1]);
+	} else {
+            setOpponentPaddle(opponentPaddlePos[0] + 984, opponentPaddlePos[1]);
+	}
 
-      break;
+	break;
     case "ball_update":
-      var pos = serverData.ball_position;
-      pos[1] = pos[1] + (topWallRect.y + topWallRect.height);
-      setBallPosition(pos);
-      break;
-  }
+	var pos = serverData.ball_position;
+	pos[1] = pos[1] + (topWallRect.y + topWallRect.height);
+	setBallPosition(pos);
+	break;
+    }
 }
 
 
 function initialize(playerNumber) {
-  playerNum = playerNumber;
-  if (playerNum == 0) {
-    initialDataPlayerOne = {
-      "phase": "initial_dimensions",
-      "name": playerName,
-      "map_dimensions": [offsetX,
-        offsetY + (topWallRect.y + topWallRect.height),
-        1024, bottomWallRect.y - (topWallRect.y + topWallRect.height)
-      ],
-      "paddle_dimensions": [paddle.x, paddle.y,
-        paddle.height, paddle.width
-      ]
-    };
-    console.log(JSON.stringify(initialDataPlayerOne));
-    send(JSON.stringify(initialDataPlayerOne));
-  } else {
-    initialDataPlayerTwo = {
-      "phase": "initial_dimensions",
-      "name": playerName,
-      "map_dimensions": [offsetX,
-        offsetY + (topWallRect.y + topWallRect.height),
-        1024, bottomWallRect.y - (topWallRect.y + topWallRect.height)
-      ],
-      "paddle_dimensions": [paddle.x + 984, paddle.y,
-        paddle.height, paddle.width
-      ]
-    };
-    console.log(JSON.stringify(initialDataPlayerTwo));
-    send(JSON.stringify(initialDataPlayerTwo));
-  }
-  
+    playerNum = playerNumber;
+    if (playerNum == 0) {
+	initialDataPlayerOne = {
+	    "phase": "initial_dimensions",
+	    "name": playerName,
+	    "map_dimensions": [offsetX,
+			       offsetY + (topWallRect.y + topWallRect.height),
+			       1024, bottomWallRect.y - (topWallRect.y + topWallRect.height)
+			      ],
+	    "paddle_dimensions": [paddle.x, paddle.y,
+				  paddle.height, paddle.width
+				 ]
+	};
+	console.log(JSON.stringify(initialDataPlayerOne));
+	send(JSON.stringify(initialDataPlayerOne));
+    } else {
+	initialDataPlayerTwo = {
+	    "phase": "initial_dimensions",
+	    "name": playerName,
+	    "map_dimensions": [offsetX,
+			       offsetY + (topWallRect.y + topWallRect.height),
+			       1024, bottomWallRect.y - (topWallRect.y + topWallRect.height)
+			      ],
+	    "paddle_dimensions": [paddle.x + 984, paddle.y,
+				  paddle.height, paddle.width
+				 ]
+	};
+	console.log(JSON.stringify(initialDataPlayerTwo));
+	send(JSON.stringify(initialDataPlayerTwo));
+    }
+    
 }
 
 function send(text) {
-  server.send('message', text);
+    server.send('message', text);
 }
 
 function connect(ipAddress, port, playerName) {
-  server = new FancyWebSocket('ws://' + ipAddress + ':' + port);
+    server = new FancyWebSocket('ws://' + ipAddress + ':' + port);
 
-  //Let the user know we're connected
-  // Server.bind('open', function() {
-  //           document.getElementById("cntBtn").disabled = true;
-  //   log( "Connected." );
-  // });
+    //Let the user know we're connected
+    // Server.bind('open', function() {
+    //           document.getElementById("cntBtn").disabled = true;
+    //   log( "Connected." );
+    // });
 
-  // //OH NOES! Disconnection occurred.
-  // Server.bind('close', function( data ) {
-  //           document.getElementById("cntBtn").disabled = false;
-  //   log( "Disconnected." );
-  // });
+    // //OH NOES! Disconnection occurred.
+    // Server.bind('close', function( data ) {
+    //           document.getElementById("cntBtn").disabled = false;
+    //   log( "Disconnected." );
+    // });
 
-  // //Log any messages sent from server
-  server.bind('message', receiveMessage);
+    // //Log any messages sent from server
+    server.bind('message', receiveMessage);
 
-  server.connect();
-  /* This send message needs to be somehwere else too, server
-  is still in connecting state at this point*/
+    server.connect();
+    /* This send message needs to be somehwere else too, server
+     is still in connecting state at this point*/
 }
 
 function disconnect(ipAddress, port, playerName) {
-  var json = {
-    "phase": "disconnect",
-    "name": playerName
-  }
-
-  server.send("message", JSON.stringify(json));
-  server.disconnect();
+    var json = {
+	"phase": "disconnect",
+	"name": playerName
+    };
+    clearInterval(updatePaddleIntervalID);
+    console.log("disconnecting");
+    server.send("message", JSON.stringify(json));
+    server.disconnect();
 
 }
